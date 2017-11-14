cf8e0167aaa52ac0f4f23da997ff0cd5ab02babf
Xingxin Morris He <30434598+XingxinMorris@users.noreply.github.com>
2017-11-14 22:05:58 +0800

    Add files via upload

---
+++ Chapter 3 Intro to List/Places I Wanna Visit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 20:59:09 2017
+
+@author: Administrator
+"""
+
+place_for_vacation = ['Vancouver','Basel','Melbourne','New York','Shanghai']
+print(sorted(place_for_vacation))
+print(place_for_vacation)
+print(sorted(place_for_vacation,reverse=True))
+print(place_for_vacation)
+place_for_vacation.reverse()
+print(place_for_vacation)
+place_for_vacation.reverse()
+print(place_for_vacation)
+place_for_vacation.sort()
+print(place_for_vacation)
+place_for_vacation.sort(reverse=True)
+print(place_for_vacation)
