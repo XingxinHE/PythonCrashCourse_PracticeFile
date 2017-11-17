8f6c3c8f28df9445f08b4c4e8b11ccdddf17d4f5
Xingxin Morris He <30434598+XingxinMorris@users.noreply.github.com>
2017-11-17 18:25:55 +0800

    Add files via upload

---
+++ Chapter 5 If Statement/alien color-ages-fruits-if practice.py
@@ -0,0 +1,30 @@
+alien_color = 'red'
+if alien_color == 'green':
+    print('You have got 5 points!')
+elif alien_color == 'yellow':
+    print('You have got 10 points!')
+else:
+    print('You have got 15 points!')
+
+age = 10
+if age < 2:
+    print('You are a baby!')
+elif age < 4:
+    print('You are learning walking!')
+elif age < 13:
+    print('You are a child!')
+elif age < 20:
+    print('You are a teenager!')
+elif age < 65:
+    print('You have grown up!')
+else:
+    print('You are an elder citizen!')
+
+favorite_fruits = ['watermelon','apple','orange']
+if 'watermelon' in favorite_fruits:
+    print('You really like watermelon!')
+if 'apple' in favorite_fruits:
+    print('You really like apple!')
+if 'orange' in favorite_fruits:
+    print('You really like orange!')
+    
