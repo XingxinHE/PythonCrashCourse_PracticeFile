f8e3277aa09229412ab97a3d0662882065d6d7ef
Xingxin Morris He <30434598+XingxinMorris@users.noreply.github.com>
2017-11-18 17:16:27 +0800

    Add files via upload

---
+++ Chapter 6 Dictionary/dic_person_num_voc.py
@@ -0,0 +1,11 @@
+marcusLeung = {
+        'First Name' : 'Yiming',
+        'Surname' : 'Leung',
+        'age' : 23,
+        'city' : 'Yangjiang'
+        }
+
+print (marcusLeung['First Name'])
+print (marcusLeung['Surname'])
+print (marcusLeung['age'])
+print (marcusLeung['city'])
